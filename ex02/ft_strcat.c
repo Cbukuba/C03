73f74ce6c5abb4c3d414d8b3cb5e8ded361dbc90
cbukuba <cbukuba@student.s19.be>
2021-09-16 12:28:02 +0000

    Exercise02v2

--- ex02/ft_strcat.c
+++ ex02/ft_strcat.c
@@ -10,22 +10,22 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-char    *ft_strcat(char *dest, char *src)
+char	*ft_strcat(char *dest, char *src)
 {
-    int    i;
-    int    j;
+	int	i;
+	int	j;
 
-    i = 0;
-    while (dest[i] != '\0')
-        i ++;
-    j = 0;
-    while (src[j] != '\0')
-    {
-        dest[i + j] = src[j];
-        j++;
-    }
-    dest[i] = '\0';
-    return (dest);
+	i = 0;
+	while (dest[i] != '\0')
+		i ++;
+	j = 0;
+	while (src[j] != '\0')
+	{
+		dest[i + j] = src[j];
+		j++;
+	}
+	dest[i + j] = '\0';
+	return (dest);
 }
 
 /*int main()
