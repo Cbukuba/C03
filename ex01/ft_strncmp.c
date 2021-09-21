dddacf2b6f1744d377b45096fb01182f48151b01
cbukuba <cbukuba@student.s19.be>
2021-09-21 08:10:21 +0000

    Exercise01trick

--- ex01/ft_strncmp.c
+++ ex01/ft_strncmp.c
@@ -10,25 +10,52 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_strncmp(char *s1, char *s2, unsigned int n)
+// int	ft_strncmp(char *s1, char *s2, unsigned int n)
+// {
+// 	int				i;
+// 	unsigned int	cmp;
+
+// 	i = 0;
+// 	cmp = 1;
+// 	while (s1[i] == s2[i] && s1[i] != '\0' && s2[i] != '\0' && cmp < n)
+// 	{
+// 		i ++;
+// 		cmp ++;
+// 	}
+// 	return (s1[i] - s2[i]);
+// }
+// #include <string.h>
+// #include <stdio.h>
+// int main()
+// {
+//     char s1[] = "aaei";
+//     char s2[] = "abed";
+//     printf("%d\n", strncmp(s1, s2, 6));
+// 	printf("%d", ft_strncmp(s1, s2, 6));
+// }
+
+int    ft_strncmp(char *s1, char *s2, unsigned int n)
 {
-	int				i;
-	unsigned int	cmp;
+    int                i;
+    unsigned int    cmp;
 
-	i = 0;
-	cmp = 1;
-	while (s1[i] == s2[i] && s1[i] != '\0' && s2[i] != '\0' && cmp < n)
-	{
-		i ++;
-		cmp ++;
-	}
-	return (s1[i] - s2[i]);
+    i = 0;
+    cmp = 0;
+    while (s1[i] == s2[i] && s1[i] != '\0' && s2[i] != '\0' && cmp < n)
+    {
+        i ++;
+        cmp ++;
+    }
+    if (s2[i] > s1[i])
+      return (-(n - cmp));
+    return (n - cmp);
 }
-
-/*int main()
+#include <string.h>
+#include <stdio.h>
+int main()
 {
     char s1[] = "abei";
     char s2[] = "abed";
-    int result = ft_strncmp(s1, s2, 5);
-    printf("%d", result);
-}*/
+    printf("%d\n", strncmp(s1, s2, 4));
+    printf("%d", ft_strncmp(s1, s2, 4));
+}
